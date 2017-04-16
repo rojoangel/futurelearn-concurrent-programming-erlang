ed12ae933476370309c2db732d8514dd5d64ac7e
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-04-16 15:20:40 +0200

    modify client to deal with server being killed

--- w2/hardening-the-frequency-server/client.erl
+++ w2/hardening-the-frequency-server/client.erl
@@ -1,11 +1,14 @@
 -module(client).
 -export([start/0,loop/2]).
+-export([init/0]).
 
 % Initializes a client with random parameters
 start() ->
-    spawn(client,
-          loop,
-          [rand:uniform(10) * 1000, rand:uniform(5) *1000]).
+    spawn(client, init, []).
+
+init() ->
+    process_flag(trap_exit,true),
+    loop(rand:uniform(10) * 1000, rand:uniform(5) *1000).
 
 % Client logic
 % Allocates a frequency, sleeps and deallocates it
@@ -19,15 +22,39 @@ start() ->
 % > client:start().
 
 loop(TimeBetweenAllocations, TimeToKeepFreq) ->
-  case frequency:allocate() of
+  case allocate() of
     {ok, Freq} ->
       io:format("~w allocated ~w~n", [self(), Freq]),
       timer:sleep(TimeToKeepFreq),
-      frequency:deallocate(Freq),
+      deallocate(Freq, TimeBetweenAllocations, TimeToKeepFreq),
       io:format("~w deallocated ~w~n", [self(), Freq]);
     {error, no_frequency} ->
-      io:format("~w failed to allocate a frequency~n", [self()])
+      io:format("~w failed to allocate a frequency~n", [self()]);
+    {error, server_down} -> % Handle fequency server down
+      io:format("frequency server down. Sleeping...~n")
   end,
   timer:sleep(TimeBetweenAllocations),
   loop(TimeToKeepFreq, TimeBetweenAllocations).
-    
+
+
+% Override deallocate to deal with 'EXIT' message  
+deallocate(Freq, TimeBetweenAllocations, TimeToKeepFreq) ->
+    receive
+        {'EXIT', _Pid, _Reason} -> % Checking for 'EXIT' msg before deallocating
+            io:format("frequency server exited. Sleeping...~n"),
+            timer:sleep(TimeBetweenAllocations), % Sleeping
+            loop(TimeBetweenAllocations, TimeToKeepFreq) % loop as there's no need to deallocate
+    after 0 -> % if there are no 'EXIT' msgs in the mailbox normal deallocate process
+        frequency:deallocate(Freq)
+    end.
+
+% Override allocate to deal with server down
+allocate() ->
+    try frequency:allocate() of 
+        Reply -> 
+            Reply
+    catch
+        _Error:_Reason -> 
+            {error, server_down}
+    end.
+                
