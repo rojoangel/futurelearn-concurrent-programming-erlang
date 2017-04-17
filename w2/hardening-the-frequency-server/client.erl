1e3aa5cd74cb0562425482971d75e40f55f1b110
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-04-17 11:47:42 +0200

    add ability to shutdown the whole system

--- w2/hardening-the-frequency-server/client.erl
+++ w2/hardening-the-frequency-server/client.erl
@@ -24,40 +24,60 @@ init() ->
 % > client:stop().
 
 loop(TimeBetweenAllocations, TimeToKeepFreq) ->
-  case allocate() of
-    {ok, Freq} ->
-      io:format("~w allocated ~w~n", [self(), Freq]),
-      timer:sleep(TimeToKeepFreq),
-      case deallocate(Freq) of 
-          ok -> 
-              io:format("~w deallocated ~w~n", [self(), Freq]);
-          killed ->
-              io:format("frequency server killed. Sleeping...~n")
-      end;
-    {error, no_frequency} ->
-      io:format("~w failed to allocate a frequency~n", [self()]);
-    {error, server_down} -> % Handle fequency server down
-      io:format("frequency server down. Sleeping...~n")
-  end,
-  timer:sleep(TimeBetweenAllocations),
-  loop(TimeToKeepFreq, TimeBetweenAllocations).
-
+    timer:sleep(TimeBetweenAllocations),
+    case allocate() of
+        {ok, Freq} ->
+            io:format("~w allocated ~w~n", [self(), Freq]),
+            timer:sleep(TimeToKeepFreq),
+            case deallocate(Freq) of 
+                ok -> 
+                    io:format("~w deallocated ~w~n", [self(), Freq]),
+                    loop(TimeToKeepFreq, TimeBetweenAllocations);
+                killed ->
+                    io:format("~w Sleeping... frequency server killed~n", [self()]),
+                    loop(TimeToKeepFreq, TimeBetweenAllocations);
+                stopped ->
+                    io:format("~w exiting - server stopped~n", [self()]),
+                    exit(self(), normal)
+            end;
+        {error, no_frequency} ->
+            io:format("~w Sleeping... failed to allocate a frequency~n", [self()]),
+            loop(TimeBetweenAllocations, TimeToKeepFreq);
+        {error, server_down} -> % Handle fequency server down
+            io:format("~w Sleeping... frequency server down~n,", [self()]),
+            loop(TimeBetweenAllocations, TimeToKeepFreq);
+        stopped -> 
+            io:format("~w exiting - server stopped~n", [self()]),
+            exit(self(), normal);
+        killed ->
+            io:format("~w Sleeping... frequency server killed~n", [self()]),
+            loop(TimeToKeepFreq, TimeBetweenAllocations)
+    end.
 
 % Override deallocate to deal with 'EXIT' message  
 deallocate(Freq) ->
     receive
         {'EXIT', _Pid, killed} -> % Checking for 'EXIT' msg before deallocating
-            killed
+            killed;
+        {'EXIT', _Pid, normal} ->
+            stopped
     after 0 -> % if there are no 'EXIT' msgs in the mailbox normal deallocate process
         frequency:deallocate(Freq)
     end.
 
 % Override allocate to deal with server down
 allocate() ->
-    try frequency:allocate() of 
-        Reply -> 
-            Reply
-    catch
-        _Error:_Reason -> 
-            {error, server_down}
+    receive
+        {'EXIT', _Pid, killed} -> % Checking for 'EXIT' msg before deallocating
+            killed;
+        {'EXIT', _Pid, normal} ->
+            stopped
+    after 0 -> 
+        try frequency:allocate() of 
+            Reply -> 
+                Reply
+        catch
+            _Error:_Reason -> 
+                {error, server_down}
+        end
     end.
